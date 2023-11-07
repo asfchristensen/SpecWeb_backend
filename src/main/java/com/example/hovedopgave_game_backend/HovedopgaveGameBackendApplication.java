a3d2bd218c555aa6c78c503d40c75623d1f2a09e
rasmuswoldike <rswoldike@gmail.com>
2023-11-07 13:21:22 +0100

    dependencies

---
+++ src/main/java/com/example/hovedopgave_game_backend/HovedopgaveGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hovedopgave_game_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HovedopgaveGameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HovedopgaveGameBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hovedopgave_game_backend/HovedopgaveGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hovedopgave_game_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HovedopgaveGameBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
