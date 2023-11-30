17940c80d6e9e855fe803c89bf3a614fca9e96c8
LauraStorm <lbs2rm@hotmail.com>
2023-11-30 13:18:56 +0100

    get all guesses with spectator id

--- src/main/java/com/example/hovedopgave_game_backend/controllers/GuessesController.java
+++ src/main/java/com/example/hovedopgave_game_backend/controllers/GuessesController.java
@@ -1,11 +1,15 @@
 package com.example.hovedopgave_game_backend.controllers;
 
 import com.example.hovedopgave_game_backend.models.Guesses;
+import com.example.hovedopgave_game_backend.models.Quiz;
 import com.example.hovedopgave_game_backend.services.GuessesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -28,6 +32,12 @@ public class GuessesController {
         } else {
             return new ResponseEntity<>("Failed to create Quiz: ", HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @GetMapping("/{spectatorId}")
+    public ResponseEntity getByID(@PathVariable("spectatorId") long spectatorId){
+        List<Guesses> guesses = guessesService.findBySpectatorId(spectatorId);
+        return new ResponseEntity<>(guesses, HttpStatus.OK);
     }
 
 
--- src/main/java/com/example/hovedopgave_game_backend/repositories/GuessesRepo.java
+++ src/main/java/com/example/hovedopgave_game_backend/repositories/GuessesRepo.java
@@ -3,5 +3,8 @@ package com.example.hovedopgave_game_backend.repositories;
 import com.example.hovedopgave_game_backend.models.Guesses;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GuessesRepo extends JpaRepository<Guesses, Long> {
+    List<Guesses> findBySpectatorId(long specId);
 }
